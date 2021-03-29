ed9e685ca843551d925a41cc23a0690775fb9059
Rob Sturcke <rob@rslabs.dev>
2021-03-28 23:43:19 -0400

    products setup with json placeholders for now

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import products from '../products.json';
 
 export default function Home() {
+  console.log('products', products);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>404 Skateboards</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>404 Skateboards</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>The hardest to find </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {products.map((product) => {
+            const { id, title, description, image, price } = product;
+            return (
+              <li key={id} className={styles.card}>
+                <a href="https://nextjs.org/docs">
+                  <img src={image} alt={title} />
+                  <h3>{title}</h3>
+                  <p>{price}</p>
+                  <p>{description}</p>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -61,5 +45,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
